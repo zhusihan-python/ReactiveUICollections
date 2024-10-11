c260a588c43f23aa1ed833dfb4f2ebf7281aacd7
allenz <1009698013@qq.com>
2024-10-11 16:44:57 +0800

    add project WinFormsRXUI

---
+++ WinFormsRXUI/WinFormRxUI/ViewModels/HomeViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+using ReactiveUI;
+
+
+/// <summary>
+/// ViewModel Example
+/// </summary>
+namespace WinFormRxUI.ViewModels
+{
+    public class HomeViewModel : ReactiveUI.ReactiveObject
+    {
+        string modelString = "";
+        public string EnteredText
+        {
+            get { return modelString; }
+            set { this.RaiseAndSetIfChanged(ref modelString, value); }
+        }
+
+        string statusString = "";
+        public string Status
+        {
+            get { return statusString; }
+            set { this.RaiseAndSetIfChanged(ref statusString, value); }
+        }
+
+        public ReactiveCommand<Unit, Unit> OKCmd { get; private set; }
+
+        public HomeViewModel()
+        {
+            OKCmd = ReactiveCommand.Create(() => { Status = EnteredText + " is saved."; }
+                , this.WhenAnyValue(vm => vm.EnteredText, s => !string.IsNullOrWhiteSpace(s)));
+        }
+    }
+}
